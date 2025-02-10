32c5b71490f76f863c4cd7f6a570bbd0ff67a78f
XXXXX <XXXXX@g.educaand.es>
2025-02-10 11:05:27 +0100

    cambio2

--- calculadora.js
+++ calculadora.js
@@ -7,5 +7,21 @@ function sumar(a, b) {
 function restar(a, b) {
   return a - b;
 }
+function multiplicar(a,b){
+  return a * b;
+}
+function dividir(a,b){
+  if(b == 0)
+    return  "No se puede dividir entre 0";
+  else
+  return a / b;
+}
+function raiz(a){
 
-module.exports = { sumar, restar };
+  if (a <=0)
+    return "No existen las raices negativas"
+    else
+  return Math.sqrt(a);
+}
+
+module.exports = { sumar, restar,multiplicar,dividir,raiz };
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,9 +1,32 @@
-const { sumar, restar } = require('./calculadora');
+const { sumar, restar,multiplicar,dividir,raiz } = require('./calculadora');
 
 test('Suma 2 + 3 y devuelve 5', () => {
     expect(sumar(2, 3)).toBe(5);
 });
+test('Suma 3 + 3 y devuelve 5', () => {
+    expect(sumar(3, 3)).toBe(6);
+});
 
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(restar(5, 3)).toBe(2);
+});
+
+test('Resta 6 - 3 y devuelve 3', () => {
+    expect(restar(6, 3)).toBe(3);
+});
+test('Resta 5 - 2 y devuelve 3', () => {
+    expect(restar(5, 3)).toBe(2);
+});
+
+
+
+test('Multiplica 5 * 3 y devuelve 15', () => {
+    expect(multiplicar(5, 3)).toBe(15);
+});
+test('Divide 6 / 3 y devuelve 2', () => {
+    expect(dividir(6, 3)).toBe(2);
+});
+
+test('Raiz de 4 es 2', () => {
+    expect(raiz(4)).toBe(2);
 });
